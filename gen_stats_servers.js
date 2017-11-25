820c6f790b57f858a1c2696a29d46dd87a734b07
eightsq <ok@electronvo.lt>
2017-11-25 18:55:12 +0100

    Improved stats_servers (progressive rise)

--- gen_stats_servers.js
+++ gen_stats_servers.js
@@ -5,8 +5,14 @@ const gen = require('./genesis');
 let dummyData = {};
 
 dummyData.dataPoints = [];
+
+let lastCount = 0;
+let lastTimestamp = 1511612600;
+
 for(let i = 0; i < 60; i++) {
-    dummyData.dataPoints.push({timestamp: gen.time(), serversVisited: gen.counter()});
+    dummyData.dataPoints.push({timestamp: lastTimestamp, serversVisited: lastCount});
+    lastTimestamp += Math.floor(Math.random()*3000);
+    lastCount += Math.floor(Math.random()*400);
 }
 
 console.log(JSON.stringify(dummyData));
